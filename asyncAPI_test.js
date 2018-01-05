9b7407164a25b6cf238448f548df1015c70467aa
fairlycasual <34873242+fairlycasual@users.noreply.github.com>
2018-01-04 22:50:44 -0800

    Update asyncAPI_test.js

--- asyncAPI_test.js
+++ asyncAPI_test.js
@@ -4,14 +4,23 @@ async function getObs() {
 	try {
 		let response = await fetch('http://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
 		if (response.ok) {
-			let obs = response.STATION[0].OBSERVATIONS.snow_depth_value_1;
-			console.log(obs);
+			let jsonData = await response.json();
+			//console.log(jsonData);
+			let obs = jsonData.STATION[0].OBSERVATIONS.snow_depth_value_1;
+			let snowDepth = obs.value;
+			let timeStamp = obs.date_time;
+            let string = 'The snow depth at Heather Meadows is ' + snowDepth + ' inches at ' + timeStamp;
 		}
 	throw new Error('Request Failed!');
 	} catch (error) {
 		//console.log(error);
 	}
+ var output = document.getElementById('output');
+
+ output.innerHTML = string;
 }
+ 
+getObs()
 
 getObs();
 
