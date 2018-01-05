2d7fecf1541403161ab80d543fced7e59a1af012
fairlycasual <34873242+fairlycasual@users.noreply.github.com>
2018-01-04 22:34:41 -0800

    Update asyncAPI_test.js

--- asyncAPI_test.js
+++ asyncAPI_test.js
@@ -4,9 +4,7 @@ async function getObs() {
 	try {
 		let response = await fetch('http://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
 		if (response.ok) {
-			let jsonData = await response.json();
-			//console.log(jsonData);
-			let obs = jsonData.STATION[0].OBSERVATIONS.snow_depth_value_1;
+			let obs = response.STATION[0].OBSERVATIONS.snow_depth_value_1;
 			console.log(obs);
 		}
 	throw new Error('Request Failed!');
