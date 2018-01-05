deead5cba8eda7ba188cdbfde97688f99cc9c35a
fairlycasual <34873242+fairlycasual@users.noreply.github.com>
2018-01-04 22:11:06 -0800

    Add files via upload

--- asyncAPI_test.js
+++ asyncAPI_test.js
@@ -2,19 +2,41 @@
 
 async function getObs() {
 	try {
-		let response = await fetch('https://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
+		let response = await fetch('http://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
 		if (response.ok) {
 			let jsonData = await response.json();
+			//console.log(jsonData);
 			let obs = jsonData.STATION[0].OBSERVATIONS.snow_depth_value_1;
-            		console.log(obs);
+			console.log(obs);
 		}
-	else throw new Error('Request Failed!');
+	throw new Error('Request Failed!');
 	} catch (error) {
-		console.log(error);
+		//console.log(error);
 	}
 }
 
 getObs();
 
+jsonData.onload = function heatherMeados(obs) {
+	var snowDepth = obs['value'];
+	var timeStamp = obs['date_time'];
+	console.log(snowDepth);
+	console.log(timeStamp);
 
+	for (var i = 0; i < obs.length; i++) {
+		var myArticle = document.createElement('article');
+		var myPara1 = document.createElement('p');
+		var myPara2 = document.createElement('p');
+		
 
+		myPara1.textContent = 'Current Heather Meadows snow depth is ' + snowDepth;
+		myPara2.textContent = 'as of ' + timeStamp; 
+
+		myArticle.appendChild(myPara1);
+		myArticle.appendChild(myPara2);
+
+		section.appendChild(myArticle);
+		
+		}
+
+}
