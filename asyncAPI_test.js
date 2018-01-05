3122c0ee5bb96fc5f38cf004a78a7a7397565291
fairlycasual <34873242+fairlycasual@users.noreply.github.com>
2018-01-05 14:16:45 -0800

    Update asyncAPI_test.js

--- asyncAPI_test.js
+++ asyncAPI_test.js
@@ -13,41 +13,7 @@ async function getObs() {
 		       
 			document.write(string);
 			
-			function populateTable(object) {
-		   	var obj = obs;
-		   	var table = $("<table />");
-		   	table[0].border = "1";
-		   	var columns = Object.keys(obj[0]);
-		   	var columnCount = columns.length;
-		   	var row = $(table[0].insertRow(-1));
-		   	for (var i = 0; i < obj.lenght; i++) {
-		   		var headerCell = $("<th />");
-		   		headerCell.html([columns[i]]);
-		   		row.append(headerCell);
-		   	}
-
-            for (var i = 0; i < obj.length; i++) {
-            	row = $(table[0].insertRow(-1));
-            	for (var j = 0; j < columnCount; j++) {
-            		var cell = $("<td />");
-            		cell.html(obj[i] [columns[j]]);
-            		row.append(cell);
-            	}
-            }
-
-            var dvTable = $("#dvCSV");
-            dvTable.html("");
-            dvTable.append(table);
-
-        }
-
-        populateTable(obs)
-
-		   }
-
-
-			
-		
+		}	
 	throw new Error('Request Failed!');
 	} catch (error) {
 		//console.log(error);
