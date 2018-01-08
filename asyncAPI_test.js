57c5730e2539f849e880f87c714cbc452c3bc5c6
fairlycasual <34873242+fairlycasual@users.noreply.github.com>
2018-01-08 15:32:13 -0800

    Update asyncAPI_test.js

--- asyncAPI_test.js
+++ asyncAPI_test.js
@@ -2,7 +2,7 @@
 
 async function getObs() {
 	try {
-		let response = await fetch('http://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
+		let response = await fetch('https://api.mesowest.net/v2/stations/nearesttime?&stid=mtb42&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf');
 		if (response.ok) {
 			let jsonData = await response.json();
 			//console.log(jsonData);
